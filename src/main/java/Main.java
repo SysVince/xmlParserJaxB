92fa9c2413fa6e638214fc7a4403cf1edbcc36ce
sys.vince <sys.vince@gmail.com>
2022-03-30 17:45:22 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Printa ut från och till.
+        //Printa ut hur många items.
+        //Räkna ut totala priset på ordern
+
+    }
+}
